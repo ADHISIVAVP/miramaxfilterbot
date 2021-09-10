493419dd934be7a4a11bc877b5a2168daa9d7c64
morgan <88886548+ADHISIVAVP@users.noreply.github.com>
2021-09-10 12:15:04 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @miramaxflimz",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🎖️ JOIN OUR CHANNEL 🎖️ ', url="https://t.me/miramaxfilmz"
                                 )
                         ]
                     ]
